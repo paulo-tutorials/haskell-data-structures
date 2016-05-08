e34d2dc3d1b75339dba0727284b9e749870c8784
Paulo Sousa <pauloj10@gmail.com>
2016-05-08 11:00:03 -0300

    Refactor Tree to match book's notation

--- src/Tree.hs
+++ src/Tree.hs
@@ -5,33 +5,33 @@ module Tree
 , member')
 where
 
-data Tree a = Elem a (Tree a) (Tree a) | Empty 
+data Tree a = T (Tree a) a (Tree a) | E 
     deriving (Show, Ord, Eq)
 
 member :: Ord a => a -> Tree a -> Bool
-member _ Empty = False
-member a (Elem x left right)
+member _ E = False
+member a (T left x right)
     | a < x = member a left
     | a > x = member a right
     | otherwise = True
 
 insert :: Ord a => a -> Tree a -> Tree a
-insert a Empty = Elem a Empty Empty
-insert a t@(Elem x left right)
-    | a < x = Elem x (insert a left) right
-    | a > x = Elem x left (insert a right)
+insert a E = T E a E
+insert a t@(T left x right)
+    | a < x = T (insert a left) x right
+    | a > x = T left x (insert a right)
     | otherwise = t
 
 member' :: Ord a => a -> Tree a -> Bool
-member' _ Empty = False
-member' a (Elem x left right)
+member' _ E = False
+member' a (T left x right)
     | a < x = member' a left
     | otherwise = member'1 a right x
 
 member'1 :: Ord a => a -> Tree a -> a -> Bool
-member'1 a Empty candidate
+member'1 a E candidate
     | a > candidate = False
     | otherwise = True
-member'1 a (Elem x left right) candidate
+member'1 a (T left x right) candidate
     | a < x = member'1 a left candidate
     | otherwise = member'1 a right x
--- test/TreeSpec.hs
+++ test/TreeSpec.hs
@@ -8,24 +8,24 @@ spec :: Spec
 spec = context "using Set as element" $ do
     describe "verify that `a`" $ do
         it "is member of tree" $ do
-            member 1 (Elem 3 (Elem 1 Empty Empty) (Elem 7 Empty Empty))
+            member 1 (T (T E 1 E) 3 (T E 7 E))
                 `shouldBe` True
         it "is not member of tree" $ do
-            member 10 (Elem 3 (Elem 1 Empty Empty) (Elem 7 Empty Empty))
+            member 10 (T (T E 1 E) 3 (T E 7 E))
                 `shouldBe` False
     describe "insert an item a" $ do
         it "in a tree that already contain it" $ do
-            insert 7 (Elem 5 (Elem 3 Empty Empty) (Elem 15 (Elem 7 Empty Empty) Empty))
+            insert 7 (T (T E 3 E) 5 (T (T E 7 E) 15 E))
                 `shouldBe`
-                    (Elem 5 (Elem 3 Empty Empty) (Elem 15 (Elem 7 Empty Empty) Empty))
+                    (T (T E 3 E) 5 (T (T E 7 E) 15 E))
         it "in a tree that do not contain it" $ do
-            insert 0 (Elem 5 (Elem 3 Empty Empty) (Elem 15 Empty Empty))
+            insert 0 (T (T E 3 E) 5 (T (T E 7 E) 15 E))
                 `shouldBe`
-                    (Elem 5 (Elem 3 (Elem 0 Empty Empty) Empty) (Elem 15 Empty Empty))
-    describe "verify that `a` (algorithm O(d))" $ do
+                    (T (T (T E 0 E) 3 E) 5 (T (T E 7 E) 15 E))
+    describe "verify that `a` (using member d+1 worst case)" $ do
         it "is member of a tree" $ do
-            member' 1 (Elem 3 (Elem 1 Empty Empty) (Elem 7 Empty Empty))
+            member' 1 (T (T E 1 E) 3 (T E 7 E))
                 `shouldBe` True
         it "is not member of tree" $ do
-            member' 10 (Elem 3 (Elem 1 Empty Empty) (Elem 7 Empty Empty))
+            member' 10 (T (T E 1 E) 3 (T E 7 E))
                 `shouldBe` False
