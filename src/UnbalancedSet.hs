fb13772ef70e39324ad6e8d060dff56f2ffd47a3
Paulo Sousa <pauloj10@gmail.com>
2016-05-09 22:49:49 -0300

    Rename Tree to UnbalancedSet

---
+++ src/UnbalancedSet.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+module UnbalancedSet
+( UnbalancedSet(..)
+, member') 
+where
+
+import qualified Set.Set as S
+
+data UnbalancedSet a = T (UnbalancedSet a) a (UnbalancedSet a) | E 
+    deriving (Show, Ord, Eq)
+
+instance (Ord a) => S.Set (UnbalancedSet a) a where
+    empty = E
+    
+    insert a E = T E a E
+    insert a t@(T left x right)
+        | a < x = T (S.insert a left) x right
+        | a > x = T left x (S.insert a right)
+        | otherwise = t
+
+    member _ E = False
+    member a (T left x right)
+        | a < x = S.member a left
+        | a > x = S.member a right
+        | otherwise = True
+
+-- exercise 2.2
+member' :: (Ord a) => a -> UnbalancedSet a -> Bool
+member' _ E = False
+member' a (T left x right)
+    | a < x = S.member a left 
+    | otherwise = member'1 a right x
+    where   member'1 b E candidate
+                | b > candidate = False
+                | otherwise = True
+            member'1 b (T left y right) candidate
+                | b < y = member'1 b left candidate
+                | otherwise = member'1 b right y
---
+++ test/UnbalancedSetSpec.hs
@@ -0,0 +1,32 @@
+module UnbalancedSetSpec where
+
+import Test.Hspec
+
+import UnbalancedSet
+import qualified Set.Set as S
+
+spec :: Spec
+spec = context "using Set as element" $ do
+    describe "verify that `a`" $ do
+        it "is member of UnbalancedSet" $ do
+            S.member 1 (T (T E 1 E) 3 (T E 7 E))
+                `shouldBe` True
+        it "is not member of UnbalancedSet" $ do
+            S.member 10 (T (T E 1 E) 3 (T E 7 E))
+                `shouldBe` False
+    describe "insert an item a" $ do
+        it "in a UnbalancedSet that already contain it" $ do
+            S.insert 7 (T (T E 3 E) 5 (T (T E 7 E) 15 E))
+                `shouldBe`
+                    (T (T E 3 E) 5 (T (T E 7 E) 15 E))
+        it "in a UnbalancedSet that do not contain it" $ do
+            S.insert 0 (T (T E 3 E) 5 (T (T E 7 E) 15 E))
+                `shouldBe`
+                    (T (T (T E 0 E) 3 E) 5 (T (T E 7 E) 15 E))
+    describe "verify that `a` (worst case d+1)" $ do
+        it "is member of a UnbalancedSet" $ do
+            member' 1 (T (T E 1 E) 3 (T E 7 E))
+                `shouldBe` True
+        it "is not member of UnbalancedSet" $ do
+            member' 10 (T (T E 1 E) 3 (T E 7 E))
+                `shouldBe` False
